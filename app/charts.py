c8d9223fee5a9f32de0c40fbdf69445668accad2
Luis <sluis07442@gmail.com>
2023-07-09 17:50:28 -0500

    add docs

--- app/charts.py
+++ app/charts.py
@@ -18,16 +18,3 @@ if __name__ == '__main__':
     labels = ['a', 'b', 'c']
     values = [100, 200, 80]
     generate_bar_chart(labels, values)
-
-
-# Lo comento todo porque con Python3.11 me sale errores cuando utilizo la librería Matplotlib
-# Lo pude arreglar :)
-
-# Para instalar pip en Python3.11
-# sudo apt install python3.11-venv
-# python3.11 -m ensurepip
-
-# Para verificar: python3.11 -m pip --version
-# Para instalar librería: python3.11 -m pip install (librería)
-# Para instalar última version de pip: curl -sS https://bootstrap.pypa.io/get-pip.py | python3.(x)
-# (x): la versión de Python
