0c1e190c737a661065b444052d7e5a2e06bfdbd3
Luis <sluis07442@gmail.com>
2023-07-10 21:17:09 -0500

    add comments to web-server

--- web-server/main.py
+++ web-server/main.py
@@ -3,14 +3,14 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
 
-app = FastAPI()
+app = FastAPI() #Instancia de FastAPI
 
-@app.get('/')
+@app.get('/') #Funcion decorada: Cada que se obtenga la ruta '/'se ejecutara la siguiente funcion
 def get_list():
-    return [1, 2, 3]
+    return [1, 2, 3] #Devolverá una lista de numeros
 
-@app.get('/contact', response_class=HTMLResponse)
-def get_list():
+@app.get('/contact', response_class=HTMLResponse) #Funcion decorada: Cada que se obtenga la ruta '/contact' tendra una respuesta de tipo HTMl
+def get_list():                                   #Esto es para que pueda soportar una solicitud HTML y funcione la pagina
     return """
         <h1>Hola soy una pagina</h1>
         <p>soy un parrafo</p>
